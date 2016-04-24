389adea3cc28907100844fd4d4554516f0b9b653
Yusuke Higuchi <hi12yusuke@gmail.com>
2016-04-23 22:16:56 -0700

    Add a basic logic for rolling dices. Need to add more rules.

--- main.py
+++ main.py
@@ -6,28 +6,47 @@ class Dice:
     """
     Creating a class for a dice.
     The user number initially start with a random integer between 1 and 6.
-    Then every time add_dice() is called, another random integer is added to the user number.
+    Then every time dice_play() is called, another random integer is added to the user number.
     """
     def __init__(self):
         self.user_num = random.randint(1, 6)
         self.dice_rolled = random.randint(1, 6)
 
-    def add_dice(self):
+    def dice_play(self, x):
         """ This is a function for adding numbers.
             When the function is called, the user roll another dice, and add to the current user number.
         """
-        self.user_num = self.user_num + self.dice_rolled
+        self.dice_rolled = random.randint(1, 6)
+        print "\nRolled another dice, and the number is", self.dice_rolled
+        self.user_num = x + self.dice_rolled
         return self.user_num
 
+
 dice1 = Dice()
-print dice1.user_num
+print "Rolled the first dice, and the number is", dice1.user_num
+print "Total:", dice1.user_num
 
-print "Dice rolled", dice1.dice_rolled
-new_num = dice1.add_dice()
-print new_num
+while dice1.user_num <= 15:
 
-print "Another dice rolled", dice1.dice_rolled
-new_num = dice1.add_dice()
-print new_num
+    dice1.user_num = dice1.dice_play(dice1.user_num)
+    print "Total:",
+    print dice1.user_num
 
 
+print "\nYour total number is", dice1.user_num
+
+
+if dice1.user_num == 21:
+    print "You win!"
+
+else:
+    user_input = raw_input("\nDo you want to roll a dice, or stop here?"
+                           "\nType \"No\" to stop, otherwise you roll a dice")
+
+    if user_input.lower() != "no":
+        dice1.user_num = dice1.dice_play(dice1.user_num)
+        print "Total:",
+        print dice1.user_num
+
+    else:
+        print dice1.user_num
