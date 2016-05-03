f8161c0f863afd69374b4ab0ed1e864613301e63
Yusuke Higuchi <YusukeHiguchi@yahoo.com>
2016-05-02 21:41:52 -0700

    Rewrite docstrings

--- userdice.py
+++ userdice.py
@@ -4,24 +4,49 @@ __author__ = "Yusuke Higuchi"
 
 
 class UserDice:
-    """
-    Creating a class for a dice.
-    The user_score is initially 0, and the first dice number is added to the score.
-    The dice number is always an integer between 1 and 6.
-    Then every time dice_play() is called, another random integer is added to the user_score.
+    """Contains two methods, __init__ and dice_play
+
+    The first methods __init__ creates two attributes user_score and dice_rolled.
+    For details, see "Attributes" below.
+
+    The second methods dice_play returns an integer if the method is called.
+    This method include two parameters self and x.
+    The parameter x is an integer or an accumulated user_score
+
+    Attributes:
+        user_score(int): an integers that accumulates dice numbers
+        dice_rolled(int): a random integer between 1 and 6
+
     """
 
 # this is the first method in class UserDice
     def __init__(self):
+        """Initiates the first method.
+
+        The user_score is initially 0, and the first dice number is added to the score.
+        The dice number is always an integer between 1 and 6.
+
+        """
         self.user_score = 0
         self.dice_rolled = random.randint(1, 6)
 
 # this is the second method in this UserDice
     def dice_play(self, x):
-        """
-        This is a function for playing a dice roll and adding a number to the current user score.
-        When this method is called, a random integer is assigned as dice_rolled, and then add to user_score.
-        While that integer is 6, the user is allowed to threw another dice in the same turn.
+        """Returns an integer if the method is called.
+
+        This method is used for rolling a dice.
+        When this is called, a random number is accumulated to the user_score.
+
+        In this local scope, a random integer is assigned as self.dice_rolled.
+        While self.dice_rolled is 6, the user may throw a dice without counting a turn.
+
+        :param self:
+        :type: integer:
+
+        :param x: an integer or an accumulated user_score to be added by dice_rolled
+
+        :return: self.user_score: self.user_score = x + self.dice_rolled
+
         """
 
         # a random integer is assigned to an object
@@ -55,6 +80,21 @@ class UserDice:
 
 
 def comp_dice(x):
+    """Returns an integer if the function is called
+
+    This function is used for the competitor's dice roll.
+
+    When this is called, a random number is accumulated to the comp_score.
+    A random integer is assigned as comp_dice_rolled.
+
+    When getting 6, the competitor always throws a dice without counting a turn.
+
+    :param x: an integer or an accumulated competitor's score
+    :type x: integer
+    :return: comp_score: comp_score = x + comp_dice_rolled
+    :rtype: integer
+    
+    """
 
     # a random integer is assigned to an object
     comp_dice_rolled = random.randint(1, 6)
